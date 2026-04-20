2b9a132808547f6be6f154e52dcad89e93606519
Jeff Minard <jeff@jrm.cc>
2026-04-20 11:22:53 -0700

    Use the new hall remapping to get around broken hall array

--- dash35b-simple/main-stm.lisp
+++ dash35b-simple/main-stm.lisp
@@ -82,6 +82,9 @@
 
     (print "KNIGHT ACTIVE")
 
+    ; remap the hall inputs because I blew the 5v hall array on my controller
+    (conf-remap-hall 'pin-swclk 'pin-swdio 'pin-ppm)
+
     ; turn on the 12v
     (set-aux 1 1)
 
